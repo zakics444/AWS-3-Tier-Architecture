92ac29e196ef4595e4b9e56994b859e819a9b28a
Zaki Khan <your-email@example.com>
2026-03-16 20:34:37 +0530

    adding all to the github

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'kastro2025',
+    DB_DATABASE : 'webappdb'
+});
